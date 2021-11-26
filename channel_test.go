377dd9618b6591971f56d75007666e1f5b9cb2b6
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-26 13:43:00 +0700

    Select Channel

--- channel_test.go
+++ channel_test.go
@@ -124,3 +124,35 @@ func TestRangeChannel(t *testing.T) {
 	}
 	fmt.Println("Selesai")
 }
+
+// kita bikin test channel select
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	// running goroutine
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	// select ambil data dari channel1 dan channel2
+	// kita lakukan perulangan, karena kita ingin mengambil semua data di channelnya, tapi harus pastikan kapan perulangan itu berhenti
+	// misalnya kita bikin manual counter untuk menghentikan perulangannya
+	counter := 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+
+}
