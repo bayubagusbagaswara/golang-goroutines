747d1b11bdaff449ce4df584450e39bccf42d623
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-11-27 18:31:16 +0700

    Deadlock

--- mutex_test.go
+++ mutex_test.go
@@ -76,3 +76,76 @@ func TestRWMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("Total Balance", account.GetBalance())
 }
+
+// buat type struct UserBalance
+type UserBalance struct {
+	Mutex   sync.Mutex
+	Name    string
+	Balance int
+}
+
+// method untuk struct UserBalance, fungsinya untuk Lock mutex
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+// method Unlock untuk unlock mutex
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+// buat method change, dengan parameter amount
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+// buat function Transfer dari user1 ke user2
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+
+	// kita lock dulu user1
+	user1.Lock()
+	fmt.Println("Lock user1", user1.Name)
+	// change balance user1 (dikurangi maka negatif amount)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+
+	// lock user2
+	user2.Lock()
+	fmt.Println("Lock user2", user2.Name)
+	// change balance user2 (ditambahkan amount)
+	user2.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	// unlock user1
+	user1.Unlock()
+	// unlock user2
+	user2.Unlock()
+}
+
+// buat testing deadlock
+func TestDeadLock(t *testing.T) {
+	// buat user1
+	user1 := UserBalance{
+		Name:    "Bayu",
+		Balance: 1000000,
+	}
+
+	// buat user2
+	user2 := UserBalance{
+		Name:    "Aan",
+		Balance: 1000000,
+	}
+
+	// kita coba running Transfer dengan goroutine
+	go Transfer(&user1, &user2, 100000)
+	// di kondisi lain, goroutine lain juga menjalankan transfer dari user2 ke user1
+	go Transfer(&user2, &user1, 200000)
+
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("User ", user1.Name, ", Balance ", user1.Balance)
+	fmt.Println("User ", user2.Name, ", Balance ", user2.Balance)
+
+}
